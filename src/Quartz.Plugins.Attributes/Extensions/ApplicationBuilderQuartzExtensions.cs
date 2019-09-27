f4f9dfbf9c7a35d85769055060cebac6e7e23ec6
Fu Diwei <fudiwei@sina.com>
2019-09-27 11:41:34 +0800

    init commit

---
+++ src/Quartz.Plugins.Attributes/Extensions/ApplicationBuilderQuartzExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    using Quartz.Plugins.Attributes;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ApplicationBuilderQuartzExtensions
+    {
+        /// <summary>
+        /// Start to schedule all Quartz.NET jobs.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseQuartz(this IApplicationBuilder app)
+        {
+            app.ApplicationServices.UseQuartz();
+
+            return app;
+        }
+    }
+}
---
+++ src/Quartz.Plugins.Attributes/Extensions/ServiceCollectionQuartzExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using Quartz.Plugins.Attributes;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ServiceCollectionQuartzExtensions
+    {
+        /// <summary>
+        /// Adds Quartz.NET services (SchedulerFactory, Triggers, Jobs, etc.) to the specified <see cref="IServiceCollection"/>.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddQuartz(this IServiceCollection services)
+        {
+            QuartzJobTypeContainer container = new QuartzJobTypeContainer();
+            services.AddSingleton(container);
+
+            foreach (Type type in ReflectionHelper.GetAssembliesTypes())
+            {
+                if (ReflectionHelper.IsQuartzJobClass(type) && Attribute.IsDefined(type, ReflectionHelper.TypeOfQuartzJobAttribute))
+                {
+                    services.AddTransient(type);
+                    container.Add(type);
+                }
+            }
+
+            return Quartz.DependencyInjection.Microsoft.Extensions.ServiceCollectionExtensions.AddQuartz(services);
+        }
+    }
+}
